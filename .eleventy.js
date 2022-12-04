0eb3702ca370324fc47714d1c660a5259e84e458
Oleksii Yehorchenkov <alexee@ukr.net>
2022-12-04 11:09:20 +0100

    solve problem with pdf, update 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -36,9 +36,37 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
+    // quizlet shortcode
     eleventyConfig.addShortcode("quizlet", (url) => 
         `<iframe src="${url}" height="500" width="100%" style="border:0"></iframe>`
     );
+
+    // pdf shortcode
+    // see documentation
+    // https://developer.adobe.com/document-services/docs/overview/pdf-embed-api/
+    eleventyConfig.addShortcode("pdf_embed", function(url, mode, divid) {
+        if(!divid) divid = "adobe-pdf-view";
+        if(!mode) mode = "FULL_WINDOW";
+        let filename = url.split("/").pop();
+
+        return `
+<div id="${divid}" style="height: 500px; width: 800px;"></div>
+<script src="https://documentservices.adobe.com/view-sdk/viewer.js"></script>
+<script type="text/javascript">
+    document.addEventListener("adobe_dc_view_sdk.ready", function(){
+        var adobeDCView = new AdobeDC.View({clientId: "70e713a9f68441eaa876ebca12ea8bf8", divId: "${divid}"});
+        adobeDCView.previewFile({
+            content:{ location:
+                    { url: "${url}"}},
+            metaData:{fileName: "${filename}"}
+        },
+        {
+            embedMode: "${mode}"
+        });
+    });
+</script>
+        `;
+    }); 
     
     return {
         dir: {
--- src/content/decision-makers.md
+++ src/content/decision-makers.md
@@ -5,14 +5,12 @@ layout: layouts/post.njk
 
 Embed PDF.
 
-{% pdfembed '/pdf-content/About.pdf' %}
+{% pdf_embed '/pdf-content/About.pdf' %}
 
 One more PDF
 
-{% pdfembed "https://documentservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf" "FULL_WINDOW" "adobe-pdf-2" %}
+{% pdf_embed "https://documentservices.adobe.com/view-sdk-demo/PDFs/Bodea Brochure.pdf" "FULL_WINDOW" "adobe-pdf-2" %}
 
 Embed YouTube
 
 https://www.youtube.com/watch?v=dQw4w9WgXcQ
-
-Embed Quizlet
